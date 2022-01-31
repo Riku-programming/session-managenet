c6c665b069098a7a6cd47c33ac83cb783ad08957
Riku-programming <riku_programming@gmail.com>
2022-01-31 22:37:59 +0900

    fix 環境変数のエラーを解決

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -2,20 +2,21 @@ import admin from "firebase-admin"
 
 /**
  * @description Firebaseの管理画面から取得した管理者アカウント情報
- * @note 環境変数は`.env.local`ファイルに定義しています
+ * @note 環境変数は`.env.NEXT_PUBLIClocal`ファイルに定義しています
  */
 
 const serviceAccount: admin.ServiceAccount = {
-    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    privateKey: (process.env.FIREBASE_ADMIN_PRIVATE_KEY || "").replace(
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
+    clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
+    privateKey: (process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY || "").replace(
         /\\n/g,
         "\n"
     ),
 };
 
 /**
- * @description Firebase Admin SDKを扱うためのオブジェクト
+ * @description
+ irebase Admin SDKを扱うためのオブジェクト
  * @note バックエンドのみで使用可能
  */
 
